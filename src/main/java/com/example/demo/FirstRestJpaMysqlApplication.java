70763abd57eb68f9d0f918a96763844893106da4
shifasageer <133022028+shifasageer@users.noreply.github.com>
2024-07-30 14:04:03 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/FirstRestJpaMysqlApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestJpaMysqlApplication.class, args);
+	}
+
+	
+	////Hi this is my firstRestJpa mysql projects
+	
+}
